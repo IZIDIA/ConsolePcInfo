d51a9657d9b202a718daf7544e630ad7ea137fe7
IZIDIA <ctalker-n@mail.ru>
2022-04-27 21:18:21 +0500

    add `Specs`, `Temps`, `OS`, `ActiveProcesses` info

---
+++ PcInfo/Network.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+
+namespace ConsoleGettingPcInfo.PcInfo {
+	class Network {
+		public string NetworkInterfaceName { get; set; }
+		public string IPv4 { get; set; }
+		public string MAC { get; set; }
+		public string DNS { get; set; }
+		public string DefaultGateway { get; set; }
+		public string DHCP { get; set; }
+		public Network() {
+			GetNetworkInfo();
+		}
+		public void GetNetworkInfo() {
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using ConsoleGettingPcInfo.PcInfo;
+using Newtonsoft.Json;
+using OpenHardwareMonitor.Hardware;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleGettingPcInfo {
+	class Program {
+		static void Main(string[] args) {
+			Console.WriteLine(JsonConvert.SerializeObject(new OperatingSystemInfo()));
+			Console.WriteLine(JsonConvert.SerializeObject(new Specs()));
+			Console.WriteLine(JsonConvert.SerializeObject(new Temps()));
+			Console.WriteLine(JsonConvert.SerializeObject(new ActiveProcesses()));
+			Console.ReadKey();
+		}
+	}
+}
